fbd34b8ae979622c666e1c49541955f623a6e7ef
David Han <davidhan1619@gmail.com>
2026-06-23 22:01:36 -0700

    Removing junk from package

--- Lander/Assets/Code/Lander.cs
+++ Lander/Assets/Code/Lander.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Lander : MonoBehaviour
+namespace Code
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Lander : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Update()
+        {
+            if (Keyboard.current.upArrowKey.isPressed)
+            {
+                Debug.Log("Up");
+            }
+        }
     }
 }
