ccc3e8515b9dd10c2fa38c014953ab6084d8d012
David Han <davidhan1619@gmail.com>
2026-06-23 22:37:06 -0700

    Basic controls + terrain + cinemachine for camera tracking

--- Lander/Assets/Code/Lander.cs
+++ Lander/Assets/Code/Lander.cs
@@ -5,11 +5,29 @@ namespace Code
 {
     public class Lander : MonoBehaviour
     {
-        private void Update()
+        public float mainThrust = 500;
+        public float adjustThrust = 200;
+        
+        private Rigidbody2D landerRigidBody2D;
+
+        private void Awake()
+        {
+            landerRigidBody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
         {
             if (Keyboard.current.upArrowKey.isPressed)
             {
-                Debug.Log("Up");
+                landerRigidBody2D.AddForce(transform.up * (mainThrust * Time.deltaTime));
+            }
+            if (Keyboard.current.leftArrowKey.isPressed)
+            {
+                landerRigidBody2D.AddTorque(adjustThrust * Time.deltaTime);
+            }
+            if (Keyboard.current.rightArrowKey.isPressed)
+            {
+                landerRigidBody2D.AddTorque(-adjustThrust * Time.deltaTime);
             }
         }
     }
